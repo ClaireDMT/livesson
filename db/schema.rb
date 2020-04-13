52a62cabd1ff88bd32fcb6d88dfb0e6583d9aeb5
guillaume imary <guillaume.imary@gmail.com>
2020-04-13 12:48:28 +0200

    user's fields added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_193543) do
+ActiveRecord::Schema.define(version: 2020_04_13_104626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,22 @@ ActiveRecord::Schema.define(version: 2020_04_12_193543) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "surname"
+    t.date "birthdate"
+    t.boolean "prof"
+    t.string "sex"
+    t.string "phone_number"
+    t.string "country"
+    t.text "presentation"
+    t.string "facebook"
+    t.string "twitter"
+    t.string "instagram"
+    t.string "siret_number"
+    t.text "company_address"
+    t.string "iban"
+    t.string "bic"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

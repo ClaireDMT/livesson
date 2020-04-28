d7aab862a66b771fbce72db64997063091472b18
guillaume imary <guillaume.imary@gmail.com>
2020-04-28 09:58:06 +0200

    We're good bro

--- app/models/eleve.rb
+++ app/models/eleve.rb
@@ -1,7 +1,10 @@
 class Eleve < ApplicationRecord
   belongs_to :user
   # has_one_attached :profile_picture
-  validates :name
+  validates :name, :surname, :birthdate, :sex, :phone_number, :country, presence: true
+  with_options if: :prof? do |eleve|
+    eleve.validates :city, :presentation, :siret_number, :company_address, :iban, :bic, presence: true
+  end
 
   def prof?
     prof == true

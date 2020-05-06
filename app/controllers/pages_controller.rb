07445f10b42c75377ac3a5e94658d5bb1832cb96
Marine Sourin <marinesourin@live.fr>
2020-05-06 12:21:10 +0200

    Fix controller home et erreur 500

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :home_pro, :admin]
 
   def home
-    @eleve = current_user.eleve
+    @eleve = current_user.eleve unless current_user.nil?
   end
 
   def home_pro

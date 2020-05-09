2082ed19784f56214a927ec98e04d595e5bbab2c
guillaume imary <guillaume.imary@gmail.com>
2020-05-09 12:23:06 +0200

    k

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+  # homepage
+  root to: 'pages#home'
+  # homepage prof
   get '/prof', to: 'pages#home_pro'
-  # devise_for :users
-  # root to: 'pages#home'
-  # root to: redirect('/users/sign_up')
-  # get '/inscription-confirmation', to: 'pages#inscription_confirmation'
-
-
-  get 'eleves/:id/registration', to: 'eleves#registration', as: :eleve_registration
-
+  # curstom registration controller devise
   devise_for :users, controllers: { registrations: 'customdevise/registrations' }, path: '', path_names: {
     sign_in: 'connexion',
     sign_up: 'inscription',
@@ -21,28 +17,27 @@ Rails.application.routes.draw do
     unauthenticated do
       root 'pages#home', as: :unauthenticated_root
     end
-
-  get '/home', to: 'pages#home'
-  # root to: redirect('/users/sign_up')
+  end
+  # eleve routes
   resources :eleves, only: [:new, :create, :edit, :update, :show] do
     collection do
       get '/turn_eleve_into_prof', to: 'eleves#turn_eleve_into_prof'
+      get 'eleves/:id/mes-cours', to: 'eleves#mes_cours', as: :mes_cours
+      get 'eleves/:id/mes-reservations', to: 'eleves#mes_reservations', as: :mes_reservations
     end
   end
+
+  # route pour annuler une réservation
   resources :bookings, only: :destroy
-
-  get 'eleves/:id/mes-cours', to: 'eleves#mes_cours', as: :mes_cours
-  get 'eleves/:id/mes-reservations', to: 'eleves#mes_reservations', as: :mes_reservations
-
+  # routes pour la création d'un template
   resources :templates, only: [:index, :new, :create]
+  # routes pour lessons avec l'annotation et la réservation imbriquées
   resources :lessons do
     resources :reviews, only: [:new, :create]
     resources :bookings, only: [:new, :create]
   end
-  # post 'turn_eleve_into_prof', to: 'eleves#turn_eleve_into_prof'
-# post "users/:id/reset", to: "customdevise/registrations#reset_user", as: :user_reset
-get "users/:id/mon_compte", to: "customdevise/registrations#account_settings", as: :account_settings
-patch "users/:id/update_password", to: "customdevise/registrations#update_password"
-patch "users/:id/update_email", to: "customdevise/registrations#update_email"
-  end
+  # routes pour modifier email et mdp d'un user
+  get "users/:id/mon_compte", to: "customdevise/registrations#account_settings", as: :account_settings
+  patch "users/:id/update_password", to: "customdevise/registrations#update_password"
+  patch "users/:id/update_email", to: "customdevise/registrations#update_email"
 end

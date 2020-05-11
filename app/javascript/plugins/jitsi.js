ba936a86486119c009bcfea73e4d8cc33b185a41
claire demont <claire.edith.demont@gmail.com>
2020-05-11 10:01:25 +0200

    change stream url

--- app/javascript/plugins/jitsi.js
+++ app/javascript/plugins/jitsi.js
@@ -3,14 +3,16 @@ const createLessonStream = () => {
   if(document.body.contains(lessonContainer)) {
     const domain = 'meet.jit.si';
     const lessonId = lessonContainer.dataset.lessonId;
+    const lessonProfName = lessonContainer.dataset.lessonIdProfName;
+    const lessonProfSurname = lessonContainer.dataset.lessonIdProfSurname;
+
     const options = {
-        roomName: `lessons/${lessonId}`,
+        roomName: `lessons/${lessonId}-${lessonProfName}-${lessonProfSurname}`,
         width: '100%',
         height: '100%',
         parentNode: document.querySelector('#lesson-video-container')
     };
     const api = new JitsiMeetExternalAPI(domain, options);
-    api.executeCommand('subject', );
   }
 }
 

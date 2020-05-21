c2da20021ee6cd6f1d9f3bb529354ef2872dd18c
guillaume imary <guillaume.imary@gmail.com>
2020-05-21 10:47:41 +0200

    Lien sur toutes la cards dans l'index Dropdown de la home squi fonctionne sur / et /home Modif des conditions sur le container en fonction de la page Index ne fera apparaître que les cards à partir de maintenant Date picker à partir d'aujourd'hui et pour les dates de naissance jusqu'à 12 ans Pas de 500 quand je clique sur un cours drop down style de la home

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,14 +1,28 @@
 import flatpickr from "flatpickr"
 import { French } from "flatpickr/dist/l10n/fr.js"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+let today = new Date()
+let year = today.getFullYear()
+let month = today.getMonth()
+let day = today.getDate()
+const birthDate = new Date(year - 12, month, day)
 
 flatpickr(".datepicker", {
                            dateFormat: "d-m-Y",
-                           "locale": French
+  "locale": French,
+  minDate: "today"
                          })
 
+flatpickr(".datepicker-birthdate", {
+  dateFormat: "d-m-Y",
+  "locale": French,
+  maxDate: "1 janvier 2009",
+  minDate: "1 janvier 1900"
+})
+
 flatpickr("#query_lesson_date", {
-                                  "locale": French
+                                  "locale": French,
+  minDate: "today"
                                 })
 
 flatpickr("#query_start", {

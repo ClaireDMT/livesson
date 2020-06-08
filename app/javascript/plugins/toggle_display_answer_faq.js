3e1e8aa9f96fc57284d595b778fe146004bf26d5
Marine Sourin <marinesourin@live.fr>
2020-06-08 13:35:24 +0200

    Toggle display answers + overflow

---
+++ app/javascript/plugins/toggle_display_answer_faq.js
@@ -0,0 +1,208 @@
+// Question 1
+const answer_faq1 = document.getElementById("toggleAnswerFaq1");
+const buttonAnswerFaq1 = document.getElementById("buttonToggleAnswerFaq1");
+
+if(document.body.contains(answer_faq1)) {
+  buttonAnswerFaq1.addEventListener("click", (event) => {
+    if(answer_faq1.style.display === "block"){
+      answer_faq1.style.display = "none"
+    }
+    else {
+      answer_faq1.style.display = "block"
+    }
+  });
+}
+
+// Question 2
+const answer_faq2 = document.getElementById("toggleAnswerFaq2");
+const buttonAnswerFaq2 = document.getElementById("buttonToggleAnswerFaq2");
+
+if(document.body.contains(answer_faq2)) {
+  buttonAnswerFaq2.addEventListener("click", (event) => {
+    if(answer_faq2.style.display === "block"){
+      answer_faq2.style.display = "none"
+    }
+    else {
+      answer_faq2.style.display = "block"
+    }
+  });
+}
+
+// Question 3
+const answer_faq3 = document.getElementById("toggleAnswerFaq3");
+const buttonAnswerFaq3 = document.getElementById("buttonToggleAnswerFaq3");
+
+if(document.body.contains(answer_faq3)) {
+  buttonAnswerFaq3.addEventListener("click", (event) => {
+    if(answer_faq3.style.display === "block"){
+      answer_faq3.style.display = "none"
+    }
+    else {
+      answer_faq3.style.display = "block"
+    }
+  });
+}
+// Question 4
+const answer_faq4 = document.getElementById("toggleAnswerFaq4");
+const buttonAnswerFaq4 = document.getElementById("buttonToggleAnswerFaq4");
+
+if(document.body.contains(answer_faq4)) {
+  buttonAnswerFaq4.addEventListener("click", (event) => {
+    if(answer_faq4.style.display === "block"){
+      answer_faq4.style.display = "none"
+    }
+    else {
+      answer_faq4.style.display = "block"
+    }
+  });
+}
+
+// Question 5
+const answer_faq5 = document.getElementById("toggleAnswerFaq5");
+const buttonAnswerFaq5 = document.getElementById("buttonToggleAnswerFaq5");
+
+if(document.body.contains(answer_faq5)) {
+  buttonAnswerFaq5.addEventListener("click", (event) => {
+    if(answer_faq5.style.display === "block"){
+      answer_faq5.style.display = "none"
+    }
+    else {
+      answer_faq5.style.display = "block"
+    }
+  });
+}
+
+// Question 6
+const answer_faq6 = document.getElementById("toggleAnswerFaq6");
+const buttonAnswerFaq6 = document.getElementById("buttonToggleAnswerFaq6");
+
+if(document.body.contains(answer_faq6)) {
+  buttonAnswerFaq6.addEventListener("click", (event) => {
+    if(answer_faq6.style.display === "block"){
+      answer_faq6.style.display = "none"
+    }
+    else {
+      answer_faq6.style.display = "block"
+    }
+  });
+}
+
+// Question 7
+const answer_faq7 = document.getElementById("toggleAnswerFaq7");
+const buttonAnswerFaq7 = document.getElementById("buttonToggleAnswerFaq7");
+
+if(document.body.contains(answer_faq7)) {
+  buttonAnswerFaq7.addEventListener("click", (event) => {
+    if(answer_faq7.style.display === "block"){
+      answer_faq7.style.display = "none"
+    }
+    else {
+      answer_faq7.style.display = "block"
+    }
+  });
+}
+
+// Question 8
+const answer_faq8 = document.getElementById("toggleAnswerFaq8");
+const buttonAnswerFaq8 = document.getElementById("buttonToggleAnswerFaq8");
+
+if(document.body.contains(answer_faq8)) {
+  buttonAnswerFaq8.addEventListener("click", (event) => {
+    if(answer_faq8.style.display === "block"){
+      answer_faq8.style.display = "none"
+    }
+    else {
+      answer_faq8.style.display = "block"
+    }
+  });
+}
+
+// Question 9
+const answer_faq9 = document.getElementById("toggleAnswerFaq9");
+const buttonAnswerFaq9 = document.getElementById("buttonToggleAnswerFaq9");
+
+if(document.body.contains(answer_faq9)) {
+  buttonAnswerFaq9.addEventListener("click", (event) => {
+    if(answer_faq9.style.display === "block"){
+      answer_faq9.style.display = "none"
+    }
+    else {
+      answer_faq9.style.display = "block"
+    }
+  });
+}
+
+// Question 10
+const answer_faq10 = document.getElementById("toggleAnswerFaq10");
+const buttonAnswerFaq10 = document.getElementById("buttonToggleAnswerFaq10");
+
+if(document.body.contains(answer_faq10)) {
+  buttonAnswerFaq10.addEventListener("click", (event) => {
+    if(answer_faq10.style.display === "block"){
+      answer_faq10.style.display = "none"
+    }
+    else {
+      answer_faq10.style.display = "block"
+    }
+  });
+}
+
+// Question 11
+const answer_faq11 = document.getElementById("toggleAnswerFaq11");
+const buttonAnswerFaq11 = document.getElementById("buttonToggleAnswerFaq11");
+
+if(document.body.contains(answer_faq11)) {
+  buttonAnswerFaq11.addEventListener("click", (event) => {
+    if(answer_faq11.style.display === "block"){
+      answer_faq11.style.display = "none"
+    }
+    else {
+      answer_faq11.style.display = "block"
+    }
+  });
+}
+
+// Question 12
+const answer_faq12 = document.getElementById("toggleAnswerFaq12");
+const buttonAnswerFaq12 = document.getElementById("buttonToggleAnswerFaq12");
+
+if(document.body.contains(answer_faq12)) {
+  buttonAnswerFaq12.addEventListener("click", (event) => {
+    if(answer_faq12.style.display === "block"){
+      answer_faq12.style.display = "none"
+    }
+    else {
+      answer_faq12.style.display = "block"
+    }
+  });
+}
+
+// Question 13
+const answer_faq13 = document.getElementById("toggleAnswerFaq13");
+const buttonAnswerFaq13 = document.getElementById("buttonToggleAnswerFaq13");
+
+if(document.body.contains(answer_faq13)) {
+  buttonAnswerFaq13.addEventListener("click", (event) => {
+    if(answer_faq13.style.display === "block"){
+      answer_faq13.style.display = "none"
+    }
+    else {
+      answer_faq13.style.display = "block"
+    }
+  });
+}
+
+// Question 14
+const answer_faq14 = document.getElementById("toggleAnswerFaq14");
+const buttonAnswerFaq14 = document.getElementById("buttonToggleAnswerFaq14");
+
+if(document.body.contains(answer_faq14)) {
+  buttonAnswerFaq14.addEventListener("click", (event) => {
+    if(answer_faq14.style.display === "block"){
+      answer_faq14.style.display = "none"
+    }
+    else {
+      answer_faq14.style.display = "block"
+    }
+  });
+}
